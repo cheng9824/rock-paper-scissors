e5b75568a06fa4a177119f5be13e924901a0dfc0
Cheng <yucheng9824@gmail.com>
2026-09-04 14:55:27 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -14,5 +14,36 @@ function getHumanChoice() {
     return prompt("Enter rock, paper or scissors: ", "")
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors")
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock")
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper")
+        humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock")
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper")
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors")
+        computerScore++;
+    } else {
+        console.log("You tie! try again");
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
