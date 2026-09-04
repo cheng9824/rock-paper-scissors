8119b9d9fefaabb99f42b876c75d03c070d25f5b
Cheng <yucheng9824@gmail.com>
2026-09-04 14:22:23 +0800

    Add get computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
