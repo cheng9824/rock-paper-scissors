5f5dca861f8e2d694c67636eae995409758f2c3f
Cheng <yucheng9824@gmail.com>
2026-09-07 09:56:47 +0800

    Complete rps ui exercises

--- script.js
+++ script.js
@@ -11,52 +11,39 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    const result = document.querySelector("#result");
-    console.log(humanChoice);
-
     if (humanChoice === "rock" && computerChoice === "scissors") {
-        result.textContent = "You win! Rock beats Scissors";
+        answer.textContent = "You win! Rock beats Scissors";
         humanScore++;
         hScore.textContent = humanScore;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        result.textContent = "You win! Paper beats Rock";
+        answer.textContent = "You win! Paper beats Rock";
         humanScore++;
         hScore.textContent = humanScore;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        result.textContent = "You win! Scissors beats Paper";
+        answer.textContent = "You win! Scissors beats Paper";
         humanScore++;
         hScore.textContent = humanScore;
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        result.textContent = "You lose! Paper beats Rock";
+        answer.textContent = "You lose! Paper beats Rock";
         computerScore++;
         cScore.textContent = computerScore;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        result.textContent = "You lose! Scissors beats Paper";
+        answer.textContent = "You lose! Scissors beats Paper";
         computerScore++;
         cScore.textContent = computerScore;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        result.textContent = "You lose! Rock beats Scissors";
+        answer.textContent = "You lose! Rock beats Scissors";
         computerScore++;
         cScore.textContent = computerScore;
     } else {
-        result.textContent = "You tie! try again";
+        answer.textContent = "You tie! try again";
     }
 
     if (humanScore === 5) {
         result.textContent = "Your win!";
     } else if (computerScore === 5) {
         result.textContent = "Computer win!";
-    } else {
-        result.textContent = "Tie!";
     }
-}
-
-function getHumanChoice() {
-    buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-
-        });
-    });
 }
 
 let humanScore = 0;
@@ -69,3 +56,15 @@ const cScore = document.querySelector("#computerScore");
 cScore.textContent = computerScore;
 
 const buttons = document.querySelectorAll("button");
+
+const answer = document.querySelector("#answer");
+
+const result = document.querySelector("#result");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let human = button.textContent;
+        let computer = getComputerChoice();
+        playRound(human, computer);
+    });
+});
